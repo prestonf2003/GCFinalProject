c382c66682761eb43244f4b9cde1790e65746318
rmjohns77077 <Ryanmj7707@outlook.com>
2022-06-16 15:36:23 -0400

    Created db and added scaffold

---
+++ GCFinalProject/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GCFinalProject.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Characters = new HashSet<Character>();
+        }
+
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+
+        public virtual ICollection<Character> Characters { get; set; }
+    }
+}
