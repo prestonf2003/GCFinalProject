7e907f3338c1b1ec4117ae977eb3dc58c18cf29b
prestonf2003 <33843946+prestonf2003@users.noreply.github.com>
2022-06-22 09:46:50 -0400

    Added azure changes and switch statement

--- GCFinalProject/ClientApp/src/app/showclass/showclass.component.ts
+++ GCFinalProject/ClientApp/src/app/showclass/showclass.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {DnD} from '../dnd';
 import {DndService} from '../dnd.service';
 import {Result} from '../classes';
+import { BreadcrumbModule } from 'ng-uikit-pro-standard';
 
 
 @Component({
@@ -15,6 +16,8 @@ export class ShowclassComponent implements OnInit {
  name: DnD = new DnD("", 0, "", "", [], [], [], [], [], [], "");
  characters: string[] = [];
  subclasses: string[] = [];
+ subclass: string = "";
+ Description: string = "";
 
 
   constructor(public dnd: DndService) {
@@ -27,16 +30,42 @@ export class ShowclassComponent implements OnInit {
    });
   }
   ngOnInit(): void {
+    this.DifferentClass();
   }
   GetClassByName(name: string): void{
     this.dnd.GetClassByName(name).subscribe((response) => {
      this.name = response;
      
     })
-
-
+  }
+  //  DifferentClass(): string{
+  //   switch(this.search){
+  //     case "Bard":{
+  //       this.subclass = this.subclasses[9];
+  //       this.Description = " A magical musician who supports their allies and hinders their enemies with music.";
     
+  //       break;
+  //     }
+  //     case "Barbarian": {
+  //       this.subclass = this.subclasses[0];
+  //       this.Description = "The straight forward heavy hitter. Uses large weapons to attack and is extremely durable.";
+  //       break;
+  //           }
+  //     case "Cleric": {
+  //       this.subclass = this.subclasses[8];
+  //       this.Description = "The versitile healer that can also hold their own in a fight.";
+  //       break;
+  //     }
+  //     case "Druid": {
+  //       this.subclass = this.subclasses[7];
+  //       this.Description = "An environmental caster who uses the land to help or harm. They also have the ability to shapeshift into animals for combat and mobility.";
+  //     }
+  //   }
+  //   return this.subclass;
+  //       return this.Description;
+  //   }
+
   }
 
 
-}
+
