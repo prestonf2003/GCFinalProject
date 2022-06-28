20e4a3bd170c4ad317c05387a326f9d3d084de59
rmjohns77077 <Ryanmj7707@outlook.com>
2022-06-28 16:23:45 -0400

    created the battle component

--- GCFinalProject/ClientApp/src/app/app.module.ts
+++ GCFinalProject/ClientApp/src/app/app.module.ts
@@ -9,6 +9,16 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+<<<<<<< Updated upstream
+=======
+import { ShowclassComponent } from './showclass/showclass.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { CreateCharacterComponent } from './create-character/create-character.component';
+import { ViewPresetComponent } from './view-preset/view-preset.component';
+import { SpellListComponent } from './spell-list/spell-list.component';
+import { CartoonCharComponent } from './cartoon-char/cartoon-char.component';
+import { BattleComponent } from './battle/battle.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -16,7 +26,19 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
+<<<<<<< Updated upstream
     FetchDataComponent
+=======
+    FetchDataComponent,
+    ShowclassComponent,
+    MainViewComponent,
+    CreateCharacterComponent,
+    ViewPresetComponent,
+    SpellListComponent,
+    CartoonCharComponent,
+    BattleComponent
+
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +48,16 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+<<<<<<< Updated upstream
+=======
+      { path: 'showclass', component: ShowclassComponent },
+      { path: 'create-character', component: CreateCharacterComponent },
+      { path: 'view-preset', component: ViewPresetComponent },
+      { path: 'spell-list', component: SpellListComponent },
+      { path: 'Cartoon-Combatants', component: CartoonCharComponent },
+      { path: 'battle', component: BattleComponent}
+
+>>>>>>> Stashed changes
     ])
   ],
   providers: [],
---
+++ GCFinalProject/ClientApp/src/app/cartoon-char/cartoon-char.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CharacterService } from '../character.service';
+import { CartoonChar } from '../CartoonChar';
+import { ToonServiceService } from '../toon-service.service';
+@Component({
+  selector: 'app-cartoon-char',
+  templateUrl: './cartoon-char.component.html',
+  styleUrls: ['./cartoon-char.component.css']
+})
+export class CartoonCharComponent implements OnInit {
+  currentUser = this.charService.currentUser;
+  searchedToons:CartoonChar[]= [];
+  userId: string = "";
+  search: string = "".toLowerCase();
+  constructor(public toonService: ToonServiceService, public charService: CharacterService) 
+  {
+    this.toonService.showAllCartoonCharacters().subscribe((result) => {
+      this.searchedToons = result;
+      console.log(this.searchedToons);
+    });
+    //this.showAllCartoonCharacters();
+  }
+
+  ngOnInit(): void {
+    this.currentUser = this.charService.currentUser;
+    this.userId = '';
+  }
+
+}
